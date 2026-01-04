1142b273eb6386dcbbd00a54a7f278b8230e86a7
sudo-mystic-duck <anonymsmail@tutamail.com>
2026-01-04 18:22:53 +0100

    Switching adapter from node to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: vercel()
+  }
 };
 
 export default config;
